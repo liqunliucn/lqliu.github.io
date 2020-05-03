796974043c422944d682c11ebd37772610d2c090
Liqun Liu <liuliqun@whut.edu.cn>
2020-05-03 19:28:20 +0200

    fix

---
+++ app/circle.js
@@ -0,0 +1,7 @@
+module.exports = function(x,y, node){
+    var d3 = require("d3");
+    var circle = document.createElement('div');
+    // greet.textContent = "Hi there and greetings!";
+    d3.select(node).append("circle").attr("cx", x).attr("cy", y).attr("r", 20).attr("fill", "black");
+    // return greet;
+}
--- app/main.js
+++ app/main.js
@@ -1,22 +1,50 @@
 import * as tf from '@tensorflow/tfjs'
 
 var greeter = require("./greeter.js");
+var circle = require("./circle.js");
 var d3 = require("d3");
+
+// debugger
+var svg = d3.select("#root").append("svg").attr("id", "circle")
+
+// debugger
+circle(30, 3, "#circle")
+
+circle(30, 30, "#circle")
+
+
+
 document.querySelector("#root").appendChild(greeter());
 
 // debuggers
 
 d3.select("#root").append("svg").selectAll("circle")
-    .data([32, 57, 112, 293])
+  .data([32, 57, 112, 293])
   .enter().append("circle")
-    .attr("cy", 60)
-    .attr("cx", function(d, i) { return i * 100 + 30; })
-    .attr("r", function(d) { return Math.sqrt(d); });
+  .attr("cy", 60)
+  .attr("cx", function (d, i) { return i * 100 + 30; })
+  .attr("r", function (d) { return Math.sqrt(d); });
 
-    d3.select("#root").append("svg")
-        .append("rect")
-        .attr("x", 50)
-        .attr("y", 60)
-        .attr("height", 50)
-        .attr("width", 90)
-        .attr("fill", "red")
+var abc = d3.select("#root").append("svg")
+  .append("rect")
+  .attr("x", 50)
+  .attr("y", 60)
+  .attr("height", 50)
+  .attr("width", 90)
+  .attr("fill", "red")
+
+  d3.select("#root").append("svg")
+  .append("rect")
+  .attr("x", 80)
+  .attr("y", 90)
+  .attr("height", 50)
+  .attr("width", 90)
+  .attr("fill", "black")
+
+  d3.select("#root").append("svg")
+  .append("rect")
+  .attr("x", 80)
+  .attr("y", 120)
+  .attr("height", 50)
+  .attr("width", 90)
+  .attr("fill", "green")
